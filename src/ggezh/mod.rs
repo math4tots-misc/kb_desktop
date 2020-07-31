b9f63edbc1e97652582d1b87f7c91450a700e995
Kyumin Kim <math4tots@users.noreply.github.com>
2020-07-31 11:40:38 -0700

    ggez: fontscale

--- src/ggezh/mod.rs
+++ src/ggezh/mod.rs
@@ -14,6 +14,7 @@ use ggez::{
 use std::rc::Rc;
 
 mod conv;
+mod send;
 
 use conv::*;
 
@@ -32,31 +33,7 @@ impl Handler for GgezHandler {
         }
     }
     fn send(&mut self, code: u32, args: Vec<Val>) -> Result<Val, Val> {
-        match code {
-            // draw text
-            1001 => {
-                checkargc(&args, 5)?;
-                let text = args[0].expect_string()?.clone();
-                let x = args[1].expect_number()? as f32;
-                let y = args[2].expect_number()? as f32;
-                let xscale = args[3].expect_number()? as f32;
-                let yscale = args[4].expect_number()? as f32;
-                converr(graphics::draw(
-                    &mut self.ctx,
-                    &graphics::Text::new(text.as_ref()),
-                    graphics::DrawParam::default().dest([x, y]).scale([xscale, yscale]),
-                ))?;
-                Ok(Val::Nil)
-            }
-            // clear screen
-            1002 => {
-                checkargc(&args, 1)?;
-                let color = to_color(&args[0])?;
-                graphics::clear(&mut self.ctx, color);
-                Ok(Val::Nil)
-            }
-            _ => Err(rterr!("Unrecognized code {}", code)),
-        }
+        self.send0(code, args)
     }
 }
 
@@ -170,5 +147,3 @@ fn converr<T, E: std::error::Error>(r: Result<T, E>) -> Result<T, Val> {
         Err(error) => Err(rterr!("{:?}", error)),
     }
 }
-
-
