9a99867a3da356178cc436a8449be415e6447029
Kyumin Kim <math4tots@users.noreply.github.com>
2020-07-31 18:06:54 -0700

    KeyDown/KeyUp

--- src/ggezh/mod.rs
+++ src/ggezh/mod.rs
@@ -8,11 +8,13 @@ use crate::Val;
 use crate::Vm;
 use crate::HCow;
 use ggez::{
-    event::{self, EventHandler},
+    event::{self, EventHandler, KeyMods, KeyCode},
     graphics::{self, Color, Text, TextFragment},
     Context, ContextBuilder, GameResult,
 };
 use std::rc::Rc;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 
 mod conv;
 mod send;
@@ -42,7 +44,10 @@ struct State {
     vm: Vm<GgezHandler>,
     update: Option<Rc<Code>>,
     draw: Option<Rc<Code>>,
-    sleep_dur: std::time::Duration,
+    keydown: Option<Rc<Code>>,
+    keyup: Option<Rc<Code>>,
+
+    keycode_cache: HashMap<KeyCode, RcStr>,
 }
 
 impl State {
@@ -52,6 +57,14 @@ impl State {
             Err(error) => {
                 eprintln!("{}", error.format());
                 std::process::exit(1);
+            }
+        }
+    }
+    fn translate_keycode(&mut self, keycode: KeyCode) -> RcStr {
+        match self.keycode_cache.entry(keycode) {
+            Entry::Occupied(entry) => entry.get().clone(),
+            Entry::Vacant(entry) => {
+                entry.insert(format!("{:?}", keycode).into()).clone()
             }
         }
     }
@@ -71,8 +84,26 @@ impl EventHandler for State {
             self.die_on_err(result);
             graphics::present(ctx)?;
         }
-        std::thread::sleep(self.sleep_dur);
+        std::thread::yield_now();
         Ok(())
+    }
+    fn key_down_event(&mut self, ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods, repeat: bool) {
+        if KeyCode::Escape == keycode {
+            event::quit(ctx);
+            return;
+        }
+        let keycode = self.translate_keycode(keycode);
+        if let Some(keydown) = &self.keydown {
+            let result = self.vm.applyfunc(keydown, vec![keycode.into(), repeat.into()], None);
+            self.die_on_err(result);
+        }
+    }
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods) {
+        let keycode = self.translate_keycode(keycode);
+        if let Some(keyup) = &self.keyup {
+            let result = self.vm.applyfunc(keyup, vec![keycode.into(), ], None);
+            self.die_on_err(result);
+        }
     }
 }
 
@@ -96,11 +127,15 @@ fn run(source_roots: Vec<String>, module_name: String) -> Result<(), BasicError>
     }
     let update = get_opt_callback(&mut vm, &format!("{}#Update", module_name).into())?;
     let draw = get_opt_callback(&mut vm, &format!("{}#Draw", module_name).into())?;
+    let keydown = get_opt_callback(&mut vm, &format!("{}#KeyDown", module_name).into())?;
+    let keyup = get_opt_callback(&mut vm, &format!("{}#KeyUp", module_name).into())?;
     let mut state = State {
         vm,
         update,
         draw,
-        sleep_dur: std::time::Duration::from_secs_f64(1.0 / 20.0),
+        keydown,
+        keyup,
+        keycode_cache: HashMap::new(),
     };
 
     match event::run(&mut ctx, &mut event_loop, &mut state) {
